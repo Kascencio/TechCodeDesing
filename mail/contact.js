dc2fcf7770efba05dce21def15ca73fdafec4dee
Kascencio <keduardoascencio@gmail.com>
2024-09-17 23:28:10 -0600

    Contact update

--- mail/contact.js
+++ mail/contact.js
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Disculpe " + name + ", El servicio de Correos no responde. Por favor tntente más tarde!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
