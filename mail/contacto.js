f04f2f155a41f4e1643309a33d9df05d8d36ed99
Kascencio <keduardoascencio@gmail.com>
2024-09-18 00:02:01 -0600

    implement services messaging

---
+++ mail/contacto.js
@@ -0,0 +1,46 @@
+$(function () {
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault();
+            
+            // Obtener los valores del formulario
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
+
+            // Número de WhatsApp al que se enviará el mensaje (incluye código de país)
+            var phoneNumber = "529932651089";  // Cambia este número por el que deseas
+
+            // Construir el mensaje de WhatsApp
+            var whatsappMessage = "Nombre: " + name + "%0A" +
+                                  "Correo: " + email + "%0A" +
+                                  "Asunto: " + subject + "%0A" +
+                                  "Mensaje: " + message;
+
+            // URL de WhatsApp con el número y el mensaje
+            var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + whatsappMessage;
+
+            // Redirigir al usuario a WhatsApp
+            window.open(whatsappURL, '_blank');
+
+            // Reiniciar el formulario
+            $('#contactForm').trigger("reset");
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function () {
+    $('#success').html('');
+});
